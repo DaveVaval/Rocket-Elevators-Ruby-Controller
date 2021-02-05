932e7a772cee7b99269dcd792fe03afa4afd8481
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-05 09:08:34 -0500

    Completed all of the methods, now in testing phase

--- residential_controller.rb
+++ residential_controller.rb
@@ -8,9 +8,9 @@ class Column
         @amount_of_elevators = amount_of_elevators
         @call_button_list = []
         @elevator_list = []
+        # On initiation the column will create it's call buttons
         button_floor = 1
         button_id = 1
-
         for index in 1..@amount_of_floors
             if button_floor < self.amount_of_floors
                 call_button = Call_Button.new(button_id, 'off', button_floor, 'up')
@@ -23,8 +23,61 @@ class Column
                 button_id += 1
             end 
             button_floor += 1
-        end 
+        end
+        # On initiation the column will create it's elevators
+        elevator_id = 1
+        for index in 1..@amount_of_elevators
+            elevator = Elevator.new(id, 'idle', self.amount_of_elevators, 1)
+            @elevator_list.push(elevator)
+            elevator_id += 1
+        end
     end
+    # This method will be called whenever a user requests an elevator
+    def request_elevator(requested_floor, direction)
+        elevator = find_elevator(requested_floor, direction)
+        elevator.floor_request_list.push(requested_floor)
+        elevator.move
+        elevator.open_doors
+        return elevator
+    end
+    # Finding the best elevator by comparing scores is managed by this method
+    def find_elevator(requested_floor, requested_direction)
+        elevator_info = {
+            :best_elevator => nil,
+            :best_score => 5,
+            :reference_gap => Float::INFINITY
+        }
+        @elevator_list.each do |elevator|
+            if requested_floor == elevator.current_floor and elevator.status == 'idle' and requested_direction == elevator.direction
+                elevator_info = check_elevator(1, elevator, elevator_info, requested_floor)
+            elsif requested_floor > elevator.current_floor and elevator.direction == 'up' and requested_direction == elevator.direction
+                elevator_info = check_elevator(2, elevator, elevator_info, requested_floor)
+            elsif requested_floor < elevator.current_floor and elevator.direction == 'down' and requested_direction == elevator.direction
+                elevator_info = check_elevator(2, elevator, elevator_info, requested_floor)
+            elsif elevator.status == 'idle'
+                elevator_info = check_elevator(3, elevator, elevator_info, requested_floor)
+            else
+                elevator_info = check_elevator(4, elevator, elevator_info, requested_floor)
+            end
+            return elevator_info[:best_elevator]
+        end
+    end
+
+    def check_elevator(base_score, elevator, elevator_info, floor)
+        if base_score < elevator_info[:best_score]
+            elevator_info[:best_score] = base_score
+            elevator_info[:best_elevator] = elevator
+            elevator_info[:reference_gap] = (elevator.current_floor - floor).abs
+        elsif elevator_info[:best_score] == base_score
+            if elevator_info[:reference_gap] > (elevator.current_floor - floor).abs
+            elevator_info[:best_score] = base_score
+            elevator_info[:best_elevator] = elevator
+            elevator_info[:reference_gap] = (elevator.current_floor - floor).abs
+            end
+        end
+        return elevator_info
+    end
+
 end
 
 
@@ -40,7 +93,56 @@ class Elevator
         @doors = Door.new(id, 'closed')
         @floor_button_list = []
         @floor_request_list = []
+        # On initiation the elevator will create it's own buttons
+        floor_number = 1
+        for index in 1..@amount_of_floors
+            floor_button = Floor_Request_Button.new(floor_number, 'off', floor_number)
+            @floor_button_list.push(floor_button)
+            floor_number += 1
+        end
     end
+    # This method will push the requested floor to the request list
+    # and calls the elevator to move and open it's doors 
+    def request_floor(floor)
+        @floor_request_list.push(floor)
+        sort_floor_request_list
+        move
+        open_doors
+    end 
+
+    def move()
+        while @floor_request_list.length != 0
+            destination = @floor_request_list[0]
+            @status = 'moving'
+            if @current_floor < destination
+                @direction = 'up'
+                while @current_floor < destination
+                    @current_floor += 1
+                end
+            elsif @current_floor > destination
+                @direction = 'down'
+                while @current_floor > destination
+                    @current_floor -= 1
+                end
+            end
+            @status = 'idle'
+            @floor_request_list.shift # Once the floor is reached it will delete the floor from the request List
+        end
+    end
+
+    def sort_floor_request_list()
+        if @direction == 'up'
+            @floor_request_list.sort # This will sort the request list ascending
+        else
+            @floor_request_list.sort{|a,b| b <=> a} # This will sort the request list descending
+        end
+    end
+
+    def open_doors()
+        @doors.status = 'open'
+        @doors.status = 'closed'
+    end
+
 end
 
 
@@ -79,6 +181,6 @@ end
 
 #-------------------------------------------------------------------------// Testing //----------------------------------------------------------------
 
-testColumn = Column.new(1, 'online', 10, 2)
+# testColumn = Column.new(1, 'online', 10, 2)
 
-puts testColumn.inspect
+# puts testColumn.inspect
